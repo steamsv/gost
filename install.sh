937c8a6bcdbff00344c0672d2c7122b18ff6cdec
steamsv <49923565+steamsv@users.noreply.github.com>
2022-01-25 21:44:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ mkdir /etc/gost
 cp /root/gost/gost /usr/bin/gost
 cp /root/gost/config.json /etc/gost/config.json
 cp /root/gost/gost.service /etc/systemd/system/gost.service
+chmod +x /usr/bin/gost
+systemctl start gost
+systemctl enable gost
