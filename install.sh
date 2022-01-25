46ade111c2bd5c82ddc996d82e5854ac47be5dcc
steamsv <49923565+steamsv@users.noreply.github.com>
2022-01-25 21:35:24 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum install git -y
+git clone https://github.com/steamsv/gost.git
+mkdir /etc/gost
+cp /root/gost/gost /usr/bin/gost
+cp /root/gost/config.json /etc/gost/config.json
+cp /root/gost/gost.service /etc/systemd/system/gost.service
