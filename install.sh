18148c9334baec7f944779721de803310345c5f2
steamsv <49923565+steamsv@users.noreply.github.com>
2022-01-31 14:07:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ yum install git -y
 git clone https://github.com/steamsv/gost.git
 mkdir /etc/gost
 cp /root/gost/gost /usr/bin/gost
-cp /root/gost/config.json /etc/gost/config.json
 cp /root/gost/gost.service /etc/systemd/system/gost.service
 chmod +x /usr/bin/gost
-systemctl start gost
 systemctl enable gost
