0470a5f97688cb03a458e001879385c6bae96af6
steamsv <49923565+steamsv@users.noreply.github.com>
2022-08-12 18:30:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ cp /root/gost/gost /usr/bin/gost
 cp /root/gost/gost.service /etc/systemd/system/gost.service
 chmod +x /usr/bin/gost
 systemctl enable gost
+systemctl restart gost
 
 
 #wget https://github.com/steamsv/gost/raw/main/install.sh && bash install.sh
